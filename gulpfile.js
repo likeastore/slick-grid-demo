4d6cca764d4516be0ec8ec6ebdb74339ee726b5c
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-01-13 01:21:20 +0200

    slick ui in progress

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,27 @@
 var gulp = require('gulp');
 var myth = require('gulp-myth');
 var swig = require('gulp-swig');
+var nodemon = require('gulp-nodemon');
 
 gulp.task('styles', function () {
 	gulp.src('./myth/*.css')
 		.pipe(myth())
-		.pipe(gulp.dest('./client/css/'));
+		.pipe(gulp.dest('./client/css'));
 });
 
 gulp.task('templates', function () {
 	gulp.src('./views/*.html')
-		.pipe(swig({ load_json: true }))
-		.pipe(gulp.dest('./client/'));
+		.pipe(swig({ load_json: true, defaults: { cache: false }}))
+		.pipe(gulp.dest('./client'));
 });
 
-gulp.task('server', function () {
-	require('./server');
+gulp.task('server', ['default'], function () {
+	gulp.src('./server.js').pipe(nodemon());
+	gulp.run('watch');
+});
 
-	gulp.watch('./myth/**', function () {
+gulp.task('watch', function () {
+	gulp.watch('./myth/*.css', function () {
 		gulp.run('styles');
 	});
 
